73709124de6be2d77e9bfadb497afb6823f12eb3
Muhsin Yolcu <muhsin_yolcu9234@hotmail.com>
2019-07-30 13:15:55 +0300

    Add project files.

---
+++ Singleton/CustomerManager.cs
@@ -0,0 +1,15 @@
+﻿namespace Singleton
+{
+    class CustomerManager
+    {
+        private static CustomerManager _customerManager;
+        private CustomerManager() { }
+
+        public static CustomerManager CreateAsSingleton()
+        {
+            return _customerManager ?? (_customerManager = new CustomerManager());
+        }
+
+        public void Save() => System.Console.WriteLine("Saved!!");
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cm = CustomerManager.CreateAsSingleton();
+            cm.Save();
+        }
+    }
+}
